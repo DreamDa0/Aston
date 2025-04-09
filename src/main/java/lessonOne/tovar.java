141110237b98f85329f01845a5c3f3f29bf518d8
DreamDa0 <ilaksenok@gmail.com>
2025-04-09 19:13:21 +0300

    add LessonOne on git

---
+++ src/main/java/lessonOne/tovar.java
@@ -0,0 +1,20 @@
+package Lesson_2;
+
+public class tovar {
+    public static void main(String [] args){
+        String name = "Название";
+        String date = "Дата производства";
+        String manufacture = "Производитель";
+        String country = "Страна происхождения";
+        String price = "Цена";
+        String status = "Cостояние бронирования покупателем";
+        System.out.println("name " + "date " + "manufacture " + "country " + "price " + "status");
+    }
+
+
+
+
+
+
+
+}
