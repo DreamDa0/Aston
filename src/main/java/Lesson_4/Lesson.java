4a4c92a9941cc7470fd05727c3e03fef278a5538
DreamDa0 <ilaksenok@gmail.com>
2025-04-16 23:10:53 +0300

    add Lesson_4 on git

--- src/main/java/Lesson_4/Lesson.java
+++ src/main/java/Lesson_4/Lesson.java
@@ -44,4 +44,21 @@ class Converter {
 
         return sum;
     }
+
+class ArrayIndexOutOfBoundsException extends IndexOutOfBoundsException {
+        private static final long serialVersionUID = -5116101128118950844L;
+
+
+        public ArrayIndexOutOfBoundsException() {
+            super();
+        }
+
+        public ArrayIndexOutOfBoundsException(int index) {
+            super("Array index out of range: " + index);
+        }
+
+        public ArrayIndexOutOfBoundsException(String s) {
+            super(s);
+        }
+    }
 }
